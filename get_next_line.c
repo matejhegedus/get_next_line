fbe42d1a15ce4aeefc77d77c96bab49010d9e46a
Matej Hegedus <mhegedus@c3r10p7.42vienna.com>
2024-10-11 23:49:52 +0200

    changes

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,31 @@
 /*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 22:37:56 by mhegedus          #+#    #+#             */
-/*   Updated: 2024/10/11 22:52:19 by mhegedus         ###   ########.fr       */
+/*   Updated: 2024/10/11 23:49:40 by mhegedus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <stdlib.h>
-// #include <unistd.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <stddef.h>
+#include "get_next_line.h"
 
 char *get_next_line(int fd)
 {
-	static size_t curr_pos;
+	// static size_t	curr_pos;
+	char			*buf;
+
+	buf = malloc(BUFFER_SIZE);
+	if (buf == NULL)
+		return (NULL);
+	read(fd, buf, BUFFER_SIZE);
+	return (buf);
+}
+
+#include <fcntl.h>
+int main(void)
+{
+	int fd = open("./test/test.txt", O_RDONLY);
+	write(1, get_next_line(fd), BUFFER_SIZE);
+	close(fd);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 22:37:39 by mhegedus          #+#    #+#             */
-/*   Updated: 2024/10/11 22:37:40 by mhegedus         ###   ########.fr       */
+/*   Updated: 2024/10/11 23:44:44 by mhegedus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
 
 char	*get_next_line(int fd);
 

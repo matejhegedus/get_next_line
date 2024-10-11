d1de8ff9d15827eab2614f91f0c130f33e899b8b
Matej Hegedus <mhegedus@c3r10p7.42vienna.com>
2024-10-11 22:54:40 +0200

    file structure

---
+++ get_next_line.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 22:37:56 by mhegedus          #+#    #+#             */
+/*   Updated: 2024/10/11 22:52:19 by mhegedus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdlib.h>
+// #include <unistd.h>
+#include <stddef.h>
+
+char *get_next_line(int fd)
+{
+	static size_t curr_pos;
+}

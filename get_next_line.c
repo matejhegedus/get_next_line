bcafab5f202b9620ca9d30560a5fcd2266fb29b6
Matej Hegedus <mhegedus@c3r10p7.42vienna.com>
2024-10-13 16:13:31 +0200

    changes

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,45 @@
 /*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 22:37:56 by mhegedus          #+#    #+#             */
-/*   Updated: 2024/10/11 23:49:40 by mhegedus         ###   ########.fr       */
+/*   Updated: 2024/10/13 16:12:39 by mhegedus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <stddef.h>
+#include <stdbool.h>
 #include "get_next_line.h"
 
 char *get_next_line(int fd)
 {
-	// static size_t	curr_pos;
-	char			*buf;
+	char	*buf;
+	size_t	ln_len;
+	size_t	i;
+	bool	is_eol;
+	char	*result;
+
 
 	buf = malloc(BUFFER_SIZE);
 	if (buf == NULL)
 		return (NULL);
+	is_eol = false;
 	read(fd, buf, BUFFER_SIZE);
+	i = 0;
+	while (i < BUFFER_SIZE)
+	{
+		ln_len++;
+		if(buf[i] == '\n')
+		{
+			is_eol = true;
+			break;
+		}
+	}
+	
+	result = malloc(ln_len);
+	if (result == NULL)
+		return (NULL);
+	
 	return (buf);
 }
 
---
+++ get_next_line_utils.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhegedus <mhegedus@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 16:11:41 by mhegedus          #+#    #+#             */
+/*   Updated: 2024/10/13 16:12:20 by mhegedus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(const char *s)
+{
+	size_t	len;
+	char	*new_s;
+	size_t	i;
+
+	len = ft_strlen(s);
+	new_s = malloc(sizeof(char) * (len + 1));
+	if (new_s == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		new_s[i] = s[i];
+		i++;
+	}
+	new_s[i] = '\0';
+	return (new_s);
+}
